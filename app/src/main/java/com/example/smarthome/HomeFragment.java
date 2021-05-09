2c768e96ff7b0db9b23c0dd4f0947f90d4d3e214
oohoangoopro <long.ho460@hcmut.edu.vn>
2021-05-09 16:27:55 +0700

    home ui

--- app/src/main/java/com/example/smarthome/HomeFragment.java
+++ app/src/main/java/com/example/smarthome/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.smarthome;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,12 +12,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import org.jetbrains.annotations.NotNull;
+
 public class HomeFragment extends Fragment {
+
+    View view;
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        addControls();
+        init();
+        return view;
     }
+
+    private void init() {
+        collapsingToolbarLayout.setTitle("Good Morning");
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.rgb(238,238,238));
+        collapsingToolbarLayout.setExpandedTitleColor(Color.rgb(238,238,238));
+    }
+
+    private void addControls() {
+        collapsingToolbarLayout = view.findViewById(R.id.collapseToolbarHome);
+    }
+
+//    @Override
+//    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+////        super.onCreateOptionsMenu(menu, inflater);
+//        inflater.inflate(R.menu.action_bar, menu);
+//    }
+
 }
