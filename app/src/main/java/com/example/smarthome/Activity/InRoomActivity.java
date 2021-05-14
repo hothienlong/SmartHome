4cbe4400a13885a3c42260fd10ad7372386949fc
oohoangoopro <long.ho460@hcmut.edu.vn>
2021-05-14 10:07:30 +0700

    fake data light screen

--- app/src/main/java/com/example/smarthome/Activity/InRoomActivity.java
+++ app/src/main/java/com/example/smarthome/Activity/InRoomActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.smarthome.R;
 
 public class InRoomActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    RelativeLayout relativeLayoutLight, relativeLayoutDoor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +37,19 @@ public class InRoomActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        relativeLayoutLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InRoomActivity.this, LightActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void addControls() {
         toolbar = findViewById(R.id.inRoomToolbar);
+        relativeLayoutLight = findViewById(R.id.relativeLayoutLight);
+        relativeLayoutDoor = findViewById(R.id.relativeLayoutDoor);
     }
 }
--- app/src/main/java/com/example/smarthome/Activity/LightActivity.java
+++ app/src/main/java/com/example/smarthome/Activity/LightActivity.java
@@ -1,16 +1,103 @@
 package com.example.smarthome.Activity;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.smarthome.Adaper.LightAdapter;
+import com.example.smarthome.Adaper.ListRoomBigAdapter;
+import com.example.smarthome.Model.Light;
+import com.example.smarthome.Model.Room;
 import com.example.smarthome.R;
 
-public class LightActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class LightActivity extends AppCompatActivity implements LightAdapter.LightClickListener {
+
+    Toolbar toolbar;
+    RecyclerView recyclerViewLight;
+    TextView tvDevicesOn;
+    ArrayList<Light> lstLight = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
+
+        addControls();
+        init();
+        addEvents();
     }
+
+    private void addEvents() {
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true); // icon
+        actionbar.setTitle("");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        // Recycler view light -----------
+        lstLight = new ArrayList<>();
+        lstLight.add(new Light("1", "Đèn trần 1", true));
+        lstLight.add(new Light("2", "Đèn trần 2", false));
+        lstLight.add(new Light("3", "Đèn học", false));
+        lstLight.add(new Light("4", "Đèn đầu giường", true));
+
+        lstLight.add(new Light("5", "Đèn trần 1", true));
+        lstLight.add(new Light("6", "Đèn trần 2", true));
+        lstLight.add(new Light("7", "Đèn học", false));
+        lstLight.add(new Light("8", "Đèn đầu giường", true));
+
+        lstLight.add(new Light("9", "Đèn trần 1", false));
+        lstLight.add(new Light("10", "Đèn trần 2", false));
+        lstLight.add(new Light("11", "Đèn học", true));
+        lstLight.add(new Light("12", "Đèn đầu giường", true));
+
+        // tạo adapter
+        LightAdapter lightAdapter = new LightAdapter(lstLight);
+        // performance
+        recyclerViewLight.setHasFixedSize(true);
+        // set adapter cho Recycler View
+        recyclerViewLight.setAdapter(lightAdapter);
+
+        onLightClick();
+        lightAdapter.setmLightClickListener(this);
+    }
+
+    private void addControls() {
+        recyclerViewLight = findViewById(R.id.recyclerViewLight);
+        toolbar = findViewById(R.id.lightToolbar);
+        tvDevicesOn = findViewById(R.id.tvDevicesOn);
+    }
+
+    @Override
+    public void onLightClick() {
+        int counter = 0;
+        for (Light light: lstLight) {
+            if (light.getStatus().equals(true))
+                counter++;
+        }
+
+        tvDevicesOn.setText("Devices on: " + counter + "/" + lstLight.size());
+    }
+
 }
