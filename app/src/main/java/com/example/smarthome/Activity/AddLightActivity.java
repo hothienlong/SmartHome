230a4ddc9100ae87e03d86db770bed4a1be3c112
oohoangoopro <long.ho460@hcmut.edu.vn>
2021-05-14 11:11:38 +0700

    add light toolbar

--- app/src/main/java/com/example/smarthome/Activity/AddLightActivity.java
+++ app/src/main/java/com/example/smarthome/Activity/AddLightActivity.java
@@ -1,16 +1,42 @@
 package com.example.smarthome.Activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.smarthome.R;
 
 public class AddLightActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_light);
+
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true); // icon
+        actionbar.setTitle("");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void addControls() {
+        toolbar = findViewById(R.id.addLightToolbar);
     }
 }
