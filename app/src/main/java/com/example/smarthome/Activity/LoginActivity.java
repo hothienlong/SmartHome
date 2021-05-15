b85fcdb46d41e53f3ad9046fa727dc751141d1b0
oohoangoopro <long.ho460@hcmut.edu.vn>
2021-05-15 15:06:52 +0700

    refactor file structure

--- app/src/main/java/com/example/smarthome/Activity/LoginActivity.java
+++ app/src/main/java/com/example/smarthome/Activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.smarthome;
+package com.example.smarthome.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.smarthome.R;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/smarthome/Activity/MainActivity.java
+++ app/src/main/java/com/example/smarthome/Activity/MainActivity.java
@@ -1,17 +1,17 @@
-package com.example.smarthome;
+package com.example.smarthome.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.smarthome.R;
 
 public class MainActivity extends AppCompatActivity {
 
